770c5fb388c5c3fb14e2a25c14fd5744ec7bec93
happygod119 <happ0983493655@gmail.com>
2022-03-08 12:33:20 +0800

    feat:配對成功 / 失敗

--- main.js
+++ main.js
@@ -27,9 +27,9 @@ const view = {
     const number = this.transformNumber((index % 13) + 1);
     const symbol = Symbols[Math.floor(index / 13)];
     return `
-    <p>${number}</p>
-    <img src="${symbol}"/>
-    <p>${number}</p>
+      <p>${number}</p>
+      <img src="${symbol}" />
+      <p>${number}</p>
     `;
   },
   //- 特殊數字轉換
@@ -55,50 +55,79 @@ const view = {
   },
   //- 翻牌
   flipCard(card) {
-    console.log(card);
     if (card.classList.contains("back")) {
       // 回傳正面
       card.classList.remove("back");
-      card.innerHTML = this.getCardContent(Number(card.dataset.index)); // 改這裡
+      card.innerHTML = this.getCardContent(Number(card.dataset.index));
       return;
     }
     // 回傳背面
     card.classList.add("back");
     card.innerHTML = null;
   },
+  //- 卡片底色
+  pairCard(card) {
+    card.classList.add("paired");
+  },
 };
 
 //* model
 const model = {
   revealedCards: [],
+  isRevealedCardsMatched() {
+    return (
+      this.revealedCards[0].dataset.index % 13 ===
+      this.revealedCards[1].dataset.index % 13
+    );
+  },
 };
 
 //* controller
 const controller = {
   currentState: GAME_STATE.FirstCardAwaits,
-  generateCards () {
-    view.displayCards(utility.getRandomNumberArray(52))
+  generateCards() {
+    view.displayCards(utility.getRandomNumberArray(52));
   },
-  dispatchCardAction (card) {
-    if (!card.classList.contains('back')) {
-      return
+  dispatchCardAction(card) {
+    if (!card.classList.contains("back")) {
+      return;
     }
     switch (this.currentState) {
       case GAME_STATE.FirstCardAwaits:
-        view.flipCard(card)
-        model.revealedCards.push(card)
-        this.currentState = GAME_STATE.SecondCardAwaits
-        break
+        view.flipCard(card);
+        model.revealedCards.push(card);
+        this.currentState = GAME_STATE.SecondCardAwaits;
+        break;
       case GAME_STATE.SecondCardAwaits:
-        view.flipCard(card)
-        model.revealedCards.push(card)
+        view.flipCard(card);
+        model.revealedCards.push(card);
         // 判斷配對是否成功
-        break
+        if (model.isRevealedCardsMatched()) {
+          // 配對成功
+          this.currentState = GAME_STATE.CardsMatched;
+          view.pairCard(model.revealedCards[0]);
+          view.pairCard(model.revealedCards[1]);
+          model.revealedCards = [];
+          this.currentState = GAME_STATE.FirstCardAwaits;
+        } else {
+          // 配對失敗
+          this.currentState = GAME_STATE.CardsMatchFailed;
+          setTimeout(() => {
+            view.flipCard(model.revealedCards[0]);
+            view.flipCard(model.revealedCards[1]);
+            model.revealedCards = [];
+            this.currentState = GAME_STATE.FirstCardAwaits;
+          }, 1000);
+        }
+        break;
     }
-    console.log('this.currentState', this.currentState)
-    console.log('revealedCards', model.revealedCards.map(card => card.dataset.index))
-  }
-}
+    console.log("this.currentState", this.currentState);
+    console.log(
+      "revealedCards",
+      model.revealedCards.map((card) => card.dataset.index)
+    );
+  },
+};
 
 //* 洗牌
 const utility = {
@@ -119,6 +148,6 @@ controller.generateCards();
 //- 事件監聽器
 document.querySelectorAll(".card").forEach((card) => {
   card.addEventListener("click", (event) => {
-    view.flipCard(card);
+    controller.dispatchCardAction(card);
   });
 });
